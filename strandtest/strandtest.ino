a644f54a9a3e6dd3accf0e693cafc8717a17b997
bhudson <brian.hudson@flexential.com>
2024-04-01 08:37:49 -0400

    fun.

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -1,14 +1,4 @@
-// A basic everyday NeoPixel strip test program.
 
-// NEOPIXEL BEST PRACTICES for most reliable operation:
-// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
-// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
-// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
-// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
-//   connect GROUND (-) first, then +, then data.
-// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
-//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
-// (Skipping these may work OK on your workbench but can fail in the field)
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
@@ -24,29 +14,13 @@
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-
-
 // setup() function -- runs once at startup --------------------------------
 
 void setup() {
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
-
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  randomSeed(analogRead(A0));
 }
 
 
@@ -54,28 +28,21 @@ void setup() {
 
 void loop() {
   // Fill along the length of the strip in various colors...
-  colorWipe(strip.Color(255,   0,   0), 1);
-  colorWipe(strip.Color(255,   0,   0), 1); // Red
-  colorWipe(strip.Color(  0, 255,   0), 10); // Green
-  colorWipe(strip.Color(  0,   0, 255), 10); // Blue
-  colorWipe(strip.Color(255,   128,   0), 4); // Red
-  colorWipe(strip.Color(  0, 255,   128), 5); // Green
-  colorWipe(strip.Color(  128,   0, 255), 90); // Blue
-  colorWipe(strip.Color(255,   128,   128), 20); // Red
-  colorWipe(strip.Color(  128, 255,   128), 49); // Green
-  colorWipe(strip.Color(  128,   128, 255), 100); // Blue
+  int red = random(0,255);
+  int green = random(0,255);
+  int blue = random(0,255);
+  colorWipe(strip.Color(red, green, blue), 400);
+  red = random(0,255); 
+  colorWipe(strip.Color(red, green, blue), 400);
+  green = random(0,255); 
+  colorWipe(strip.Color(red, green, blue), 400);
+  blue = random(0,255);
+  colorWipe(strip.Color(red, green, blue), 400);
   rainbow(60);             // Flowing rainbow cycle along the whole strip
   theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
 }
 
 
-// Some functions of our own for creating animated effects -----------------
-
-// Fill strip pixels one after another with a color. Strip is NOT cleared
-// first; anything there will be covered pixel by pixel. Pass in color
-// (as a single 'packed' 32-bit value, which you can get by calling
-// strip.Color(red, green, blue) as shown in the loop() function above),
-// and a delay time (in milliseconds) between pixels.
 void colorWipe(uint32_t color, int wait) {
   for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
     strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
